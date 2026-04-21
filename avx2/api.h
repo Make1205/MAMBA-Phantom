153a94f3aae2623e229e206dfe8d9ff0d35ba43a
Make <71177618+Make1205@users.noreply.github.com>
2026-04-21 21:48:41 +0800

    Implement MAMBA-Phantom key format and public-path flow

--- avx2/api.h
+++ avx2/api.h
@@ -4,8 +4,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define pqcrystals_dilithium2_PUBLICKEYBYTES 1312
-#define pqcrystals_dilithium2_SECRETKEYBYTES 2560
+#define pqcrystals_dilithium2_PUBLICKEYBYTES 1440
+#define pqcrystals_dilithium2_SECRETKEYBYTES 480
 #define pqcrystals_dilithium2_BYTES 2420
 
 #define pqcrystals_dilithium2_avx2_PUBLICKEYBYTES pqcrystals_dilithium2_PUBLICKEYBYTES
@@ -36,7 +36,7 @@ int pqcrystals_dilithium2_avx2_open(uint8_t *m, size_t *mlen,
 
 
 #define pqcrystals_dilithium3_PUBLICKEYBYTES 1952
-#define pqcrystals_dilithium3_SECRETKEYBYTES 4032
+#define pqcrystals_dilithium3_SECRETKEYBYTES 736
 #define pqcrystals_dilithium3_BYTES 3309
 
 #define pqcrystals_dilithium3_avx2_PUBLICKEYBYTES pqcrystals_dilithium3_PUBLICKEYBYTES
@@ -67,7 +67,7 @@ int pqcrystals_dilithium3_avx2_open(uint8_t *m, size_t *mlen,
 
 
 #define pqcrystals_dilithium5_PUBLICKEYBYTES 2592
-#define pqcrystals_dilithium5_SECRETKEYBYTES 4896
+#define pqcrystals_dilithium5_SECRETKEYBYTES 768
 #define pqcrystals_dilithium5_BYTES 4627
 
 #define pqcrystals_dilithium5_avx2_PUBLICKEYBYTES pqcrystals_dilithium5_PUBLICKEYBYTES
